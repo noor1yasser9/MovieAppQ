82b29e29c9cf3d28d93da4db82afd8bb9251235a
OMEN <mrh61037@gmail.com>
2021-03-15 12:17:51 +0200

    add view model file add splash view model

---
+++ app/src/main/java/com/nurbk/ps/movieappq/viewmodel/SplashViewModel.kt
@@ -0,0 +1,31 @@
+package com.nurbk.ps.movieappq.viewmodel
+
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+@HiltViewModel
+class SplashViewModel @Inject constructor(application: Application) :
+    AndroidViewModel(application) {
+
+    private val mutableLiveData = MutableLiveData<SplashState>()
+    val liveData: LiveData<SplashState>
+        get() = mutableLiveData
+
+    init {
+        GlobalScope.launch {
+            delay(3000)
+            mutableLiveData.postValue(SplashState.SplashFragment)
+        }
+    }
+
+    sealed class SplashState {
+        object SplashFragment : SplashState()
+    }
+}
