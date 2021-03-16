b67a1892a3961115284305170cc144a2d7507558
OMEN <mrh61037@gmail.com>
2021-03-16 15:29:25 +0200

    update moview Respoistories

--- app/src/main/java/com/nurbk/ps/movieappq/repositories/MoviesRepositories.kt
+++ app/src/main/java/com/nurbk/ps/movieappq/repositories/MoviesRepositories.kt
@@ -1,10 +1,9 @@
 package com.nurbk.ps.movieappq.repositories
 
 
-import com.nurbk.ps.movieappq.model.newMovie.NewPlaying
 import com.nurbk.ps.movieappq.model.newMovie.ResultMovie
 import com.nurbk.ps.movieappq.network.MoviesInterface
-import com.nurbk.ps.movieappq.others.ResultResponse
+import com.nurbk.ps.movieappq.utils.ResultResponse
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.MutableStateFlow
