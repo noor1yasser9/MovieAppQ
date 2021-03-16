9de69796d25cc6080104b8c9b2f8f67e569fe407
OMEN <mrh61037@gmail.com>
2021-03-16 15:30:35 +0200

    update holder adpater

--- app/src/main/java/com/nurbk/ps/movieappq/utils/HolderAdapter.kt
+++ app/src/main/java/com/nurbk/ps/movieappq/utils/HolderAdapter.kt
@@ -1,11 +1,15 @@
-package com.nurbk.ps.movieappq.others
+package com.nurbk.ps.movieappq.utils
 
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.databinding.BindingAdapter
 
 import com.bumptech.glide.Glide
-import java.text.ParseException
+import com.bumptech.glide.load.engine.DiskCacheStrategy
+import com.bumptech.glide.load.resource.bitmap.CenterCrop
+import com.bumptech.glide.load.resource.bitmap.RoundedCorners
+import com.bumptech.glide.request.RequestOptions
+import com.nurbk.ps.movieappq.others.StringConstants
 import java.text.SimpleDateFormat
 import java.util.*
 
@@ -14,8 +18,16 @@ object HolderAdapter {
     @JvmStatic
     @BindingAdapter("loadImage")
     fun loadImage(view: ImageView, imageUrl: String?) {
-        Glide.with(view.context)
+//        Glide.with(view.context)
+//            .load(StringConstants.imageUrl + imageUrl)
+//            .into(view)
+
+        Glide
+            .with(view.context)
             .load(StringConstants.imageUrl + imageUrl)
+//            .placeholder(R.drawable.placeholder)
+            .diskCacheStrategy(DiskCacheStrategy.ALL)
+            .apply(RequestOptions().transform(CenterCrop(), RoundedCorners(32 )))
             .into(view)
     }
 
