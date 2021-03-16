0e72d8c97afd9ffa77af4501d014bfb1e848fff9
OMEN <mrh61037@gmail.com>
2021-03-16 15:30:18 +0200

    add basic view Pager Transormation

---
+++ app/src/main/java/com/nurbk/ps/movieappq/utils/BasicViewPagerTransformation.kt
@@ -0,0 +1,15 @@
+package com.nurbk.ps.movieappq.utils
+
+import android.view.View
+import androidx.viewpager.widget.ViewPager
+
+class BasicViewPagerTransformation : ViewPager.PageTransformer {
+    override fun transformPage(page: View, position: Float) {
+        val absPosition = Math.abs(position)
+        if (absPosition >= 1) {
+            page.scaleY = 0.85f
+        } else {
+            page.scaleY = (0.85f - 1) * absPosition + 1
+        }
+    }
+}
