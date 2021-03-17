1853fab36059670758c142b9979a810bf1200382
OMEN <mrh61037@gmail.com>
2021-03-17 19:51:45 +0200

    update movie pager adapter

--- app/src/main/java/com/nurbk/ps/movieappq/adapter/MoviePagerAdapter.kt
+++ app/src/main/java/com/nurbk/ps/movieappq/adapter/MoviePagerAdapter.kt
@@ -8,17 +8,17 @@ import androidx.databinding.DataBindingUtil
 import androidx.viewpager.widget.PagerAdapter
 import com.nurbk.ps.movieappq.BR
 import com.nurbk.ps.movieappq.R
+import com.nurbk.ps.movieappq.databinding.ItemMovieBinding
 import com.nurbk.ps.movieappq.databinding.ItemMovieLargeBinding
 import com.nurbk.ps.movieappq.model.newMovie.ResultMovie
 
-class MoviePagerAdapter(context: Context) : PagerAdapter() {
-    private var movieItem = emptyList<ResultMovie>()
+class MoviePagerAdapter(private val itemType: ITEM_TYPE) : PagerAdapter() {
+    var movieItem = emptyList<ResultMovie>()
 
     fun setItem(movieItem: List<ResultMovie>) {
         this.movieItem = movieItem
     }
 
-    var inflater: LayoutInflater = LayoutInflater.from(context)
 
     override fun isViewFromObject(view: View, `object`: Any): Boolean {
         return view == `object`
@@ -29,8 +29,18 @@ class MoviePagerAdapter(context: Context) : PagerAdapter() {
     }
 
     override fun instantiateItem(container: ViewGroup, position: Int): Any {
-        val binding =
-         DataBindingUtil.inflate<ItemMovieLargeBinding>(inflater, R.layout.item_movie_large, container, false)
+        val binding = if (itemType == ITEM_TYPE.SMALL)
+            DataBindingUtil.inflate<ItemMovieBinding>(
+                LayoutInflater.from(container.context),
+                R.layout.item_movie,
+                container,
+                false
+            ) else
+            DataBindingUtil.inflate<ItemMovieLargeBinding>(
+                LayoutInflater.from(container.context),
+                R.layout.item_movie_large,
+                container,
+                false)
         binding.setVariable(BR.movie, movieItem[position])
         container.addView(binding.root)
 
@@ -43,7 +53,9 @@ class MoviePagerAdapter(context: Context) : PagerAdapter() {
     override fun destroyItem(container: ViewGroup, position: Int, `object`: Any) {
         container.removeView(`object` as View)
     }
-
+    override fun getPageWidth(position: Int): Float {
+        return if (itemType == ITEM_TYPE.SMALL) 0.29f else 1.0f
+    }
 
     enum class ITEM_TYPE {
         SMALL,
