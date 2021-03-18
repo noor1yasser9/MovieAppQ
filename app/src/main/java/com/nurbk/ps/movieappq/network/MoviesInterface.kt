f073d26334c3c06dab407874d07828a716fb456f
OMEN <mrh61037@gmail.com>
2021-03-18 20:43:42 +0200

    update MoviesInterface.kt

--- app/src/main/java/com/nurbk/ps/movieappq/network/MoviesInterface.kt
+++ app/src/main/java/com/nurbk/ps/movieappq/network/MoviesInterface.kt
@@ -3,11 +3,14 @@ package com.nurbk.ps.movieappq.network
 import com.nurbk.ps.movieappq.model.newMovie.NewPlaying
 import retrofit2.Response
 import retrofit2.http.GET
+import retrofit2.http.Path
 import retrofit2.http.Query
 
 interface MoviesInterface {
 
-    //Movie
-    @GET("movie/now_playing")
-    suspend fun getNowPlayingMovie(@Query("page") page: Int = 1): Response<NewPlaying>
+    @GET("movie/{type}")
+    suspend fun getNowPlayingMovie(
+        @Path("type") type: String,
+        @Query("page") page: Int = 1
+    ): Response<NewPlaying>
 }
