da6cb23e005c9efc826472be7e4b34fdf5f822d8
OMEN <mrh61037@gmail.com>
2021-03-18 20:44:22 +0200

    update home view model

--- app/src/main/java/com/nurbk/ps/movieappq/viewmodel/HomeViewModel.kt
+++ app/src/main/java/com/nurbk/ps/movieappq/viewmodel/HomeViewModel.kt
@@ -2,11 +2,11 @@ package com.nurbk.ps.movieappq.viewmodel
 
 import android.app.Application
 import androidx.lifecycle.AndroidViewModel
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.viewModelScope
 import com.nurbk.ps.movieappq.repositories.MoviesRepositories
+import com.nurbk.ps.movieappq.utils.ResultResponse
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
@@ -17,18 +17,36 @@ class HomeViewModel @Inject constructor(
 ) : AndroidViewModel(application) {
 
     private var page = 1
-    fun getAllMovie() {
+    fun getTopMovie() {
         viewModelScope.launch {
-            moviesRepositories.getAllNewMovie(page)
+            moviesRepositories.getTopMovie(page)
             page++
         }
     }
 
-    init {
-        getAllMovie()
+    fun getNewMovie() {
+        viewModelScope.launch {
+            moviesRepositories.getNewMovie()
+        }
     }
 
-    fun getMovieLiveData() = moviesRepositories.getMovieLiveData()
+    fun getUpcomingMovie() {
+        viewModelScope.launch {
+            moviesRepositories.getUpcomingMovie(1)
+        }
+    }
 
+    init {
+        getTopMovie()
+        getNewMovie()
+//        getUpcomingMovie()
+    }
 
+    fun getMovieTopLiveData() = moviesRepositories.getMovieTopLiveData()
+
+    fun getNewMovieLiveData(): StateFlow<ResultResponse<Any>> =
+        moviesRepositories.getNewMovieLiveData()
+
+    fun getUpcomingMovieLiveData(): StateFlow<ResultResponse<Any>> =
+        moviesRepositories.getUpcomingMovieLiveData()
 }
