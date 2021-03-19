82511851d825545a1fe92ce9bcffe17ca486e93b
OMEN <mrh61037@gmail.com>
2021-03-19 20:44:17 +0200

    update see all fragment

--- app/src/main/java/com/nurbk/ps/movieappq/ui/fragment/SeeAllFragment.kt
+++ app/src/main/java/com/nurbk/ps/movieappq/ui/fragment/SeeAllFragment.kt
@@ -4,12 +4,42 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.AbsListView
+import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.lifecycleScope
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.nurbk.ps.movieappq.BR
+import com.nurbk.ps.movieappq.R
+import com.nurbk.ps.movieappq.adapter.GenericAdapter
+import com.nurbk.ps.movieappq.adapter.MoviePagerAdapter
 import com.nurbk.ps.movieappq.databinding.FragmentSeeAllBinding
+import com.nurbk.ps.movieappq.model.newMovie.NewPlaying
+import com.nurbk.ps.movieappq.model.newMovie.ResultMovie
+import com.nurbk.ps.movieappq.utils.OnScrollListener
+import com.nurbk.ps.movieappq.utils.ResultResponse
+import com.nurbk.ps.movieappq.viewmodel.HomeViewModel
+import com.nurbk.ps.movieappq.viewmodel.SeeAllViewModel
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.withContext
+import javax.inject.Inject
 
-class SeeAllFragment : Fragment() {
+class SeeAllFragment : Fragment() , GenericAdapter.OnListItemViewClickListener<ResultMovie>{
 
     private lateinit var mBinding: FragmentSeeAllBinding
+    private lateinit var bundle: Bundle
+
+    @Inject
+    lateinit var viewModel: SeeAllViewModel
+    private var isLoading = false
+    private var isLastPage = false
+    private var isScrolling = false
+
+
+    private val movieAdapter by lazy {
+        GenericAdapter(R.layout.adapter_movie, BR.movie, this)
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -19,15 +49,60 @@ class SeeAllFragment : Fragment() {
         mBinding = FragmentSeeAllBinding.inflate(inflater, container, false).apply {
             executePendingBindings()
         }
+        bundle = requireArguments()
+        mBinding.title = bundle.getString("title")!!
         return mBinding.root
     }
 
+    private val onScrollListener =
+        OnScrollListener(isLoading, isLastPage, 0, isScrolling) {
+            viewModel.getMovie(bundle.getString("type")!!)
+            isScrolling = false
+        }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-
-
+        rcData()
+        lifecycleScope.launchWhenStarted {
+            viewModel.getMovieMutableLiveData().collect {
+                withContext(Dispatchers.Main) {
+                    when (it.status) {
+                        ResultResponse.Status.LOADING -> {
+                            showProgressBar()
+                        }
+                        ResultResponse.Status.SUCCESS -> {
+                            hideProgressBar()
+                            val data = it.data as NewPlaying
+                            movieAdapter
+                        }
+                        ResultResponse.Status.ERROR -> {
+                            hideProgressBar()
+                        }
+                        else -> {
+                        }
+                    }
+                }
+            }
+        }
     }
 
+    private fun rcData() {
+        with(mBinding) {
+            rcData.apply {
+                adapter = movieAdapter
+                layoutManager = LinearLayoutManager(requireContext())
+            }
+        }
+    }
 
+    private fun hideProgressBar() {
+        isLoading = false
+    }
+
+    private fun showProgressBar() {
+        isLoading = true
+    }
+    override fun onClickItem(itemViewModel: ResultMovie, type: Int) {
+
+    }
 }
