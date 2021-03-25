a5fe48b89bec99d826d88b54e50e745aac776ebe
omarmattr <omarabomattr@gmail.com>
2021-03-25 11:38:19 +0200

    add function in MoviesInterface.kt

--- app/src/main/java/com/nurbk/ps/movieappq/network/MoviesInterface.kt
+++ app/src/main/java/com/nurbk/ps/movieappq/network/MoviesInterface.kt
@@ -1,6 +1,9 @@
 package com.nurbk.ps.movieappq.network
 
+import com.nurbk.ps.movieappq.model.creadits.Credits
+import com.nurbk.ps.movieappq.model.detailsMovie.Details
 import com.nurbk.ps.movieappq.model.newMovie.NewPlaying
+import com.nurbk.ps.movieappq.model.similar.Similar
 import retrofit2.Response
 import retrofit2.http.GET
 import retrofit2.http.Path
@@ -13,6 +16,25 @@ interface MoviesInterface {
         @Path("type") type: String,
         @Query("page") page: Int = 1
     ): Response<NewPlaying>
+
+    @GET("movie/{id}")
+    suspend fun getDetailsMovie(
+        @Path("id") id: String,
+    ): Response<Details>
+    @GET("movie/{id}/credits")
+    suspend fun getCreditsMovie(
+        @Path("id") id: String,
+    ): Response<Credits>
+
+    @GET("movie/{id}/similar")
+    suspend fun getSimilarMovie(
+        @Path("id") id: String,
+    ): Response<Similar>
+
+   @GET("movie/{id}/recommendations")
+    suspend fun getRecommendationsMovie(
+        @Path("id") id: String,
+    ): Response<Similar>
 
 
 }
