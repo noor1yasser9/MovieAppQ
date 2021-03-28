93b5b71075cf9f81cf4386be8510e185501cd556
OMEN <mrh61037@gmail.com>
2021-03-28 18:09:36 +0300

    update MovieDB.kt

--- app/src/main/java/com/nurbk/ps/movieappq/db/MovieDB.kt
+++ app/src/main/java/com/nurbk/ps/movieappq/db/MovieDB.kt
@@ -2,9 +2,11 @@ package com.nurbk.ps.movieappq.db
 
 import androidx.room.Database
 import androidx.room.RoomDatabase
+import androidx.room.TypeConverters
 import com.nurbk.ps.movieappq.model.newMovie.ResultMovie
 
-@Database(entities = [ResultMovie::class], version = 1, exportSchema = false)
+@TypeConverters(value = [ConverterDB::class])
+@Database(entities = [ResultMovie::class], version = 7, exportSchema = false)
 abstract class MovieDB : RoomDatabase() {
 
     abstract fun movieDao(): MovieDAO
